9b00a22f3e1bb271c3f5113251f143a5e9019011
Nguyễn Thái Dương <23701581.duong@student.iuh.edu.vn>
2024-08-23 15:17:21 +0700

    Update Rectangle.java

--- src/iuh/fit/se/Rectangle.java
+++ src/iuh/fit/se/Rectangle.java
@@ -13,38 +13,52 @@ package iuh.fit.se;
  * version: 1.0
  */
 public class Rectangle {
-    private double lenght;
+    private double length;
     private double width;
 
     //phuong thuc tinh dien tich
     public double getArea(){
-        return lenght * width;
+        return length * width;
     }
     //phuong thuc tinh chu vi
     public double getPerimeter() {
-        return 2 * (lenght + width);
+        return 2 * (length + width);
     }
     //property get
     public double getLenght(){
-        return lenght;
+        return length;
     }
     public double getWidth(){
         return  width;
     }
     //constructor
     public Rectangle(){
-        this.lenght = 0.0;
+        this.length = 0.0;
         this.width = 0.0;
     }
-    public Rectangle(double lenght, double width){
-        this.lenght = lenght;
+    public Rectangle(double length, double width) throws IllegalAccessException {
+        if (length <= 0 || width <= 0)
+            throw new IllegalAccessException("Length and width must be greater than 0");
+        this.length = length;
         this.width = width;
     }
     //property set
-    public void setLenght(double lenght){
-        this.lenght = lenght;
-    }
-    public void setWidth(double width){
+    public void setLength(double length) throws IllegalAccessException {
+        if (length <= 0)
+            throw new IllegalAccessException("Length must be greater than 0");
+        this.length = length;
+    }    
+    public void setWidth(double width) throws IllegalAccessException{
+        if (width <= 0)
+            throw new IllegalAccessException("Width must be greater than 0");
         this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
     }
 }
